f835e774ca98c14a1e81259ca370905cc9c5bf6b
Matthew Webecke <Matthew@webecke.com>
2025-01-10 16:37:19 -0700

    nnice

---
+++ matrix.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+
+public class matrix{
+    public static void main(String[] args) {
+        
+    }
+    Scanner matrix1 = new Scanner(System.in); //create scanner
+    System.out.println("Please enter the dimensions of two square matrices from 0 to 50");
+
+}
