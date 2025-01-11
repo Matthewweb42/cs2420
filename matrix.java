01a61e3b315d591b9f01daa0fd7cd633ab263e0a
Matthew Webecke <Matthew@webecke.com>
2025-01-10 17:05:39 -0700

    nice

--- matrix.java
+++ matrix.java
@@ -2,11 +2,33 @@ import java.util.Scanner;
 
 
 
-public class matrix{
+public class Matrix{
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); //create scanner
+        System.out.println("Please enter the dimensions of the first square matrix from 0 to 50");
         
+        int dimensions1;
+        int dimensions2;
+        boolean goodNum = false;
+
+        while (!goodNum){
+            dimensions1 = scanner.nextInt();
+            dimensions2 = scanner.nextInt();
+            if (dimensions1 <0 || dimensions1 > 50){
+                System.out.println("Invalid input. Please enter a number between 0 and 50");
+            }else if(dimensions2 <0 || dimensions2 >50){
+                System.out.println("Invalid input. Please enter a number between 0 and 50");
+            }
+            else{
+                goodNum = true;
+
+            }
+        
+        }
+   
     }
-    Scanner matrix1 = new Scanner(System.in); //create scanner
-    System.out.println("Please enter the dimensions of two square matrices from 0 to 50");
+
+    
+    
 
 }
