62adeae7e20ac92b8d8e55d054a41b209eeca975
Matthew Webecke <Matthew@webecke.com>
2025-01-27 15:31:12 -0700

    NICE STUFF

--- Assignment3/queueUsingTwoStacks.java
+++ Assignment3/queueUsingTwoStacks.java
@@ -1,6 +1,8 @@
-import java.util.Stack;
+import java.util.Scanner;
+
 
 public class queueUsingTwoStacks {
+
    private static class Node{// create the array for the first stack.
        int data;
        Node next;
@@ -12,7 +14,7 @@ public class queueUsingTwoStacks {
        }
    }
     private static class Stack{
-       Node top;
+       private Node top;
 
        public Stack(){
            top = null;
@@ -24,6 +26,7 @@ public class queueUsingTwoStacks {
                top = newNode;
            }else{
                newNode.next = top;
+               top = newNode;
            }
        }
        public int pop(){
@@ -31,13 +34,32 @@ public class queueUsingTwoStacks {
            top = top.next;
            return data;
        }
+       public boolean isEmpty(){
+           return top == null;
+       }
+
+       public void printStacks(String stack){
+            Node current = top;
+            while(current != null){
+                System.out.print(current.data + " ");
+                current = current.next;
+            }
+           System.out.println();
+        }
     }
+
 
     private Stack stack1;
     private Stack stack2;
+    public queueUsingTwoStacks(){
+        stack1 = new Stack();
+        stack2 = new Stack();
+    }
+
 
     public void enqueue(int value) {
         stack1.push(value);
+
     }
 
     public int dequeue() {
@@ -45,8 +67,35 @@ public class queueUsingTwoStacks {
             stack2.push(stack1.pop());
         }
         return stack2.pop();
+    }
+
+    public void printStacks() {
+        stack1.printStacks("Stack1");
+        stack2.printStacks("Stack2");
+    }
 
 
-        return 0;
+
+    public static void main(String[] args) {
+        queueUsingTwoStacks queue = new queueUsingTwoStacks();
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("Press 1 for enqueue. Press 2 for dequeue. Press 0 to exit");
+            int choice = scanner.nextInt();
+            if (choice == 1) {
+                System.out.println("Please input the number you would like to Enqueue: ");
+                int data = scanner.nextInt();
+                queue.enqueue(data);
+                queue.printStacks();
+            }else if(choice == 2){
+                queue.dequeue();
+                queue.printStacks();
+            }else if(choice == 0){
+                System.out.println("Exiting...");
+                exit = true;
+            }
+        }
+
     }
 }
