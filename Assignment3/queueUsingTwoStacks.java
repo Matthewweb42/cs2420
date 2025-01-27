1b4a58fa35b27eccb60ca2caff83a8fd8ce8c84b
Matthew Webecke <Matthew@webecke.com>
2025-01-27 11:13:24 -0700

    Starting

---
+++ Assignment3/queueUsingTwoStacks.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class queueUsingTwoStacks {
+    public static void main(String[] args) {
+        Stack<Integer> stack1 = new Stack<Integer>();
+        Stack<Integer> stack2 = new Stack<Integer>();
+
+    }
+
+    public int enqueue(int item) {
+        return 0;
+    }
+
+    public int dequeue() {
+        return 0;
+    }
+}
---
+++ Assignment3/searchInLinkedList.java
@@ -0,0 +1,81 @@
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class searchInLinkedList {
+//creating the list
+    static class Node {
+        int data;
+        Node next;
+        Node prev;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = null;
+        Node tail = null;
+        for (int i = 0; i < 10; i++) {
+            int value = new Random().nextInt(100); //Initialize the Linked list with random numbers
+            Node newNode = new Node(value);
+            if (head == null) { //setting head and tail to correct values
+                head = newNode;
+                tail = newNode;
+            }else{
+                tail.next = newNode;
+                newNode.prev = tail;
+                tail = newNode;
+
+            }
+        }
+
+        //printing the linked list Left to Right
+        Node current = head;
+        System.out.print("Linked list values Left to Right: ");
+        while(current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+
+        }
+        //printing right to left
+        current = tail;
+        System.out.println();
+        System.out.print("Linked list values Right to Left: ");
+        while(current != null) {
+            System.out.print(current.data + " ");
+            current = current.prev;
+        }
+
+        //Search Key input
+        System.out.println();
+        System.out.print("Please input a Search Key 0-100: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        int key = sc.nextInt();
+
+        //Search for key
+        boolean found = false;
+        current = head;
+        while(current != null) {
+            if(key == current.data) {
+                found = true;
+                break;
+            }
+            current = current.next;
+        }
+
+        //print results
+        if (found){
+            System.out.println("Key has been found!");
+        }else{
+            System.out.println("Key does not exist!");
+        }
+
+    }
+
+
+
+}
